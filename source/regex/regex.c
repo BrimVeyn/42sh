b2987c19009aa7d3c1aa1f0694720f57dd71e642
Nathan Bardavid <nbabardavid@gmail.com>
2024-08-23 13:28:13 +0200

    not stable save

--- source/regex/regex.c
+++ source/regex/regex.c
@@ -6,7 +6,7 @@
 /*   By: nbardavi <nbabardavid@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 12:10:41 by nbardavi          #+#    #+#             */
-/*   Updated: 2024/08/23 10:29:16 by nbardavi         ###   ########.fr       */
+/*   Updated: 2024/08/23 13:28:00 by nbardavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ typedef struct {
 } match_result;
 
 match_result regex_match(char *regexp, char *text) {
-    char *old_text = text;
     match_result result = { -1, -1 };
     int text_pos = 0;
 
@@ -67,46 +66,49 @@ int regex_is_range_match(char **regexp, char c){
     return (start <= c && c <= end);
 }
 
-int regex_is_range_end(char *regexp){
+int regex_find_range_end(char *regexp){
     int i = 0;
     for (; regexp[i] != ']'; i++){}
-    return i + 1;
+    return i;
 }
 
-int regex_is_range_start(char *regexp){
+int regex_find_range_start(char *regexp){
     int i = 0;
     for (; regexp[i] != '['; i--){}
     return i;
 }
 
 int regex_matchrange(char *regexp, char *text, int *text_pos){
-    int has_star = (regexp[regex_is_range_end(regexp)] == '*');
-
-    while(*regexp != ']' && *regexp){
+    int has_star = (regexp[regex_find_range_end(regexp) + 1] == '*');
+    do {
         if (regexp[1] == '-' && regex_is_range_match(&regexp, *text)){
             (*text_pos)++;
             if (has_star){
-                return regex_matchrange(regexp + regex_is_range_start(regexp), text + 1, text_pos);
+                return regex_matchrange(regexp + regex_find_range_start(regexp), text + 1, text_pos);
             }
-            return regex_matchhere(regexp + regex_is_range_end(regexp), text + 1, text_pos);
+            return regex_matchhere(regexp + regex_find_range_end(regexp) + 1, text + 1, text_pos);
         }
         if (regexp[0] == *text){
             (*text_pos)++;
             if (has_star){
-                return regex_matchrange(regexp + regex_is_range_start(regexp), text + 1, text_pos);
+                return regex_matchrange(regexp + regex_find_range_start(regexp), text + 1, text_pos);
             }
-            return regex_matchhere(regexp + regex_is_range_end(regexp), text + 1, text_pos);
+            return regex_matchhere(regexp + regex_find_range_end(regexp) + 1, text + 1, text_pos);
         }
+        if (*regexp == ']')
+            break;
         regexp++;
-    }
-    (*text_pos)++;
+    } while(*regexp != ']' && *regexp);
     if (has_star){
-        return regex_matchhere(regexp + 1, text + 1, text_pos);
+        printf("return star\n");
+        if (*text_pos != 0)
+            return regex_matchhere(regexp + 2, text, text_pos);
     }
     return 0;
 }
 
 int regex_matchhere(char *regexp, char *text, int *text_pos) {
+    printf("regexp char: %c\ntext char: %c\n", *regexp, *text);
     if (regexp[0] == '\0') {
         return 1;
     }
@@ -135,27 +137,39 @@ void regex_test(char *regexp, char *text){
         printf("%c", text[i]);
     }
     printf("\n");
+    printf("\033[0m");
 
     printf("%s\n", regexp);
     printf("==================\n");
 }
 
-int main(void) {
-    char *text = "Heeello world!";
-    regex_test("Heeello", text);
-    regex_test("Hedllo", text);
-    regex_test("Hi*", text);
-    regex_test("He*", text);
-    regex_test("He*l", text);
-    regex_test("ee", text);
-    regex_test("^ee", text);
-    regex_test("^He", text);
-    regex_test("H[A-Z]", text);
-    regex_test("H[a-z]", text);
-    regex_test("Heee[a-z]", text);
-    regex_test("H[a-z]*", text);
-    printf("Salut\n");
+#include <readline/readline.h>
+#include <readline/history.h>
 
+int main(int ac, char **av) {
+    if (ac >= 2 && !strcmp(av[1], "-i")){
+        char *input = NULL;
+        char *string = readline("enter string\n> ");
+        while((input = readline("> ")) != NULL){
+            regex_test(input, string);
+            add_history(input);
+        }
+    } else{
+        char *text = "Heeello-world! 0123456789";
+        regex_test("Heeello", text);
+        regex_test("Hedllo", text);
+        regex_test("Hi*", text);
+        regex_test("He*", text);
+        regex_test("He*l", text);
+        regex_test("ee", text);
+        regex_test("^ee", text);
+        regex_test("^He", text);
+        regex_test("H[A-Z]", text);
+        regex_test("H[a-z]", text);
+        regex_test("Heee[a-z]", text);
+        regex_test("H[a-z]*", text);
+        regex_test("[4-8]*", text);
+    }
     return 0;
 }
 
