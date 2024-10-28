88d29f1fc880536622876a6830cf994d569f4ebf
Bryan Van paemel <bryan.vanpaemel@gmail.com>
2024-10-28 16:30:15 +0100

    Experimenting with LALR(1) parsers. Trying to build a parsing table for the shell's grammar by hand

--- source/lexer/lexer_get_next_token.c
+++ source/lexer/lexer_get_next_token.c
@@ -6,7 +6,7 @@
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 16:19:12 by nbardavi          #+#    #+#             */
-/*   Updated: 2024/10/26 16:34:30 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2024/10/28 09:58:17 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -137,11 +137,7 @@ char *lex_interface(LexMode mode, void *input) {
 	return NULL;
 }
 
-void parse_linebreak() {
-	while (!ft_strcmp("\n", lex_interface(LEX_PEAK, NULL))) {
-		lex_interface(LEX_GET, NULL);
-	}
-}
+
 
 bool is_sep() {
 	char *peak = lex_interface(LEX_PEAK, NULL);
@@ -158,10 +154,11 @@ SeparatorOp parse_sep() {
 	else if (!ft_strcmp("&", sep))
 		return AMPER;
 	else if (!ft_strcmp("\n", sep))
-		return AMPER;
+		return NEWLINE;
 	else
 		return -1;
 }
+
 bool is_operator() {
 	char *peak = lex_interface(LEX_PEAK, NULL);
 	if (!ft_strcmp("&&", peak) || !ft_strcmp("||", peak)) {
@@ -196,121 +193,12 @@ bool is_compound_command() {
 	return false;
 }
 
-CompoundListNode *parse_compound_list() {
-	CompoundListNode *node = gc_unique(CompoundListNode, GC_SUBSHELL);
-	node->term = NULL;
 
-	parse_linebreak();
-
-	TermNode *term = parse_term();
-	if (!term) {
-		fatal("Compoundlist Term", 1);
-	}
-
-	node->term = term;
-	
-	if (is_sep()) {
-		parse_sep();
-	}
-	return node;
-}
-
-TermNode *parse_term() {
-	TermNode *node = gc_unique(TermNode, GC_SUBSHELL);
-	node->separator = SEMI;
-	node->next = NULL;
-	node->and_or = parse_and_or();
-
-	if (!node->and_or) {
-		fatal("Term and_or", 1);
-	}
-
-	if (is_sep()) {
-		node->separator = parse_sep();
-		parse_linebreak();
-		node->next = parse_and_or();
-	}
-	return node;
-}
-
-AndOrNode *parse_and_or() {
-	AndOrNode *node = gc_unique(AndOrNode, GC_SUBSHELL);
-	node->command = parse_command();
-	node->next = NULL;
-
-	if (!node->command) {
-		fatal("AndOr command\n", 1);
-	}
-
-	while (is_operator()) {
-		OperatorType op = parse_operator();
-		parse_linebreak();
-		AndOrNode *next_node = gc_unique(AndOrNode, GC_SUBSHELL);
-		next_node->command = parse_command();
-		next_node->operator = op;
-		next_node->next = NULL;
-
-		if (!next_node->command) {
-			fatal("AndOr commandNext\n", 1);
-		}
-	}
-	return node;
-}
-
-CommandNode *parse_command() {
-	CommandNode *node = gc_unique(CommandNode, GC_SUBSHELL);
-
-	if (is_simple_command()) {
-		node->type = SIMPLE_CMD;
-		node->simple_cmd = parse_simple_command();
-		if (!node->compound_cmd) {
-			fatal("Command simple command\n", 1);
-		}
-	} else if (is_compound_command()) {
-		node->type = COMPOUND_CMD;
-		node->compound_cmd = parse_compound_command();
-		if (!node->compound_cmd) {
-			fatal("Command compound command\n", 1);
-		}
-	} else {
-		return NULL;
-	}
-	return node;
-}
-
-SimpleCommandNode *parse_simple_command() {
-	SimpleCommandNode *node = gc_unique(SimpleCommandNode, GC_SUBSHELL);
-
-	node->word = lex_interface(LEX_GET, NULL);
-	if (!node->word) {
-		fatal("SimpleCommand word", 1);
-	}
-	return node;
-}
-
-CompoundCommandNode *parse_compound_command() {
-	CompoundCommandNode *node = gc_unique(CompoundCommandNode, GC_SUBSHELL);
-	node->type = IF_CLAUSE;
-	node->if_clause = parse_if_clause();
-	return node;
-}
-
-IfClauseNode *parse_if_clause() {
-	IfClauseNode *node = gc_unique(IfClauseNode, GC_SUBSHELL);
-
-	node->condition = parse_compound_list();
-	node->body = parse_compound_list();
-	if (!node->condition || !node->body) {
-		fatal("IfClause any", 1);
-	}
-
-	return node;
-}
 
 
 void lexer_tokenize(char *in) {
 	lex_interface(LEX_SET, in);
-	CompoundListNode *root = parse_compound_list();
+	CompoundListNode *root = NULL;
 	dprintf(2, "tok: %s\n", root->term->and_or->command->simple_cmd->word);
 	(void) root;
 	//print this shit;
