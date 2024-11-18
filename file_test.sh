ed3395a732cc6d3c7109e8680f40225c60610427
Bryan Van paemel <bryan.vanpaemel@gmail.com>
2024-11-18 11:52:05 +0100

    Added for_clause and case_clause

--- file_test.sh
+++ file_test.sh
@@ -1,9 +1,22 @@
+# a_func () {
+# 	printf salut\n;
+# }
 # salut nathan
 # { printf hey\n; echo salut; }
 # { printf yeah\n; printf hoho\n; }
 #
+#
+case 3 in
+	1) printf 1\n
+	;;
+	2|3) printf 2or3\n
+	;;
+	*) printf default\n
+	;;
+esac | cat
 
-{ sleep 2; false; } && printf one\n && printf two\n && printf three\n || printf salope\n && printf yeah\n
+#
+# { sleep 2; false; } && printf one\n && printf two\n && printf three\n || printf salope\n && printf yeah\n
 # printf hey\n && printf salut\n;
 #
 #
