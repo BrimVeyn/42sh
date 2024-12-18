eb4149e5712d8d3985924e24b8e6f8c7f5e6f360
Bryan Van paemel <bryan.vanpaemel@gmail.com>
2024-12-18 16:53:08 +0100

    Add(tester) Tweaked some more styling

--- vm/42sh_tester/src/Category.js
+++ vm/42sh_tester/src/Category.js
@@ -2,15 +2,17 @@ import React, {useState} from 'react';
 import UnitTest from './UnitTest';
 import Resume from './Resume';
 
-function Category({ name, passed, total, inject }) {
+function Category({ name, testsInfo, inject }) {
 
-	const categoryStyle = "w-1/4 flex flex-col justify-center text-center border-2 border-zinc-300 hover:bg-zinc-100 hover:text-blue-500 hover:scale-105 hover:shadow-lg transition-transform duration-200"
+	const categoryStyle = "w-1/4 flex flex-col justify-center text-center border-2 border-zinc-300 hover:bg-gray-600 hover:text-blue-500 hover:scale-105 hover:shadow-lg transition-transform duration-200"
 
 	return (
 		<>
 			<div className={categoryStyle} onClick={inject}>
-					<p className="text-xs overflow-hidden" >{name} </p>
-					<p> {passed} / {total} </p>
+				<p className="m-1 text-xs" >
+					{name.replace(/_/g, ' ').toUpperCase()} 
+				</p>
+				<p> {testsInfo.passed} / {testsInfo.total} </p>
 			</div>
 		</>
 	);
