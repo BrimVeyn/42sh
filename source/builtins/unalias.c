54a0b6007de955b5e89237229aa76253d0946b51
Bryan VAN PAEMEL <bryan.vanpaemel@gmail.com>
2025-01-01 17:30:55 +0100

    Fix(alias) Added a return when a invalid options is detected

--- source/builtins/unalias.c
+++ source/builtins/unalias.c
@@ -6,7 +6,7 @@
 /*   By: bvan-pae <bryan.vanpaemel@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 19:35:57 by bvan-pae          #+#    #+#             */
-/*   Updated: 2024/12/31 13:27:05 by bvan-pae         ###   ########.fr       */
+/*   Updated: 2025/01/01 17:30:02 by bvan-pae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,9 @@ void builtin_unalias(const SimpleCommandP *command, Vars * const shell_vars) {
 				} else {
 					ft_dprintf(STDERR_FILENO, "42sh: unalias: -%c: invalid option\n", arg[i]);
 					g_exitno = 2;
-					break;
+					return;
 				}
 			}
-			if (g_exitno == 2)
-				break;
 		}
 		if (erase_all) {
 			string_list_clear(shell_vars->alias);
