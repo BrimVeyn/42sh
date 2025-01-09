073d55cb76509b4014300238d404040548c91ce0
Bryan VAN PAEMEL <bryan.vanpaemel@gmail.com>
2025-01-09 07:23:16 +0000

    Fix(process_substitution) It seems to be working as expected. We'll add tests to confirm

--- source/expansions/process_substitution.c
+++ source/expansions/process_substitution.c
@@ -15,68 +15,75 @@
 #include <signal.h>
 
 char *process_substitution(char *const str, const ExpKind kind, Vars *const shell_vars, int *const error) {
-	char file_name[] = "/tmp/process_sub_XXXXXX";
-
-	int fd = mkstemp(file_name);
-	if (fd == -1)
-		_fatal("mkstemp: failed", 1);
-	close(fd);
-	unlink(file_name);
-
-
-	if (mkfifo(file_name, 0666) == -1)
-		_fatal("mkfifo: failed", 1);
-
-	pid_t pid = fork();
-	if (pid == -1) {
-		unlink(file_name); // Clean up FIFO
-		_fatal("fork: failed", 1);
-	}
-	int fifo_fd = open(file_name, O_RDWR);
-
-	if (IS_CHILD(pid)) {
-		// Set process group for job control
-		if (setpgid(0, 0) == -1) {
-			perror("setpgid");
-			unlink(file_name);
-			_fatal("setpgid: failed", 1);
-		}
-
-		// Ignore SIGTTOU to prevent stopping
-		signal(SIGTTOU, SIG_IGN);
-
-		if (kind == EXP_PROC_SUB_OUT) {
-			// Child will read from FIFO
-			if (fifo_fd == -1 || dup2(fifo_fd, STDIN_FILENO) == -1) {
-				perror("open");
-				unlink(file_name);
-				_fatal("open: failed", 1);
-			}
-			close(fifo_fd);
-
-		} else if (kind == EXP_PROC_SUB_IN) {
-			// Child will write to FIFO
-			int fifo_fd = open(file_name, O_RDWR);
-			if (fifo_fd == -1 || dup2(fifo_fd, STDOUT_FILENO) == -1) {
-				perror("open");
-				unlink(file_name);
-				_fatal("open: failed", 1);
-			}
-			close(fifo_fd);
-		}
-
-		// Execute the provided command
-		shell(SHELL_GET)->interactive = false;
-		parse_input(str, "process_sub", shell_vars);
-		exit_clean();
-	} else {
-		close(fifo_fd);
-	}
-	
+    int pipe_fd[2]; // Array to hold the pipe file descriptors
+    if (pipe(pipe_fd) == -1) {
+        perror("pipe");
+        _fatal("pipe: failed", 1);
+    }
 	(void)error;
 
-	// Parent process returns the FIFO path for substitution
-	return ft_strdup(file_name);
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        _fatal("fork: failed", 1);
+    }
+
+    if (IS_CHILD(pid)) {
+        // Child process
+        signal(SIGINT, SIG_IGN);
+
+        if (kind == EXP_PROC_SUB_OUT) {
+            // Redirect child process's output to the writing end of the pipe
+            close(pipe_fd[1]); // Close reading end
+            if (dup2(pipe_fd[0], STDIN_FILENO) == -1) {
+                perror("dup2");
+                close(pipe_fd[1]);
+                _fatal("dup2: failed", 1);
+            }
+			/*int fdnull = open("/dev/null", 0666);*/
+			/*dup2(fdnull, );*/
+			/*close(fdnull);*/
+            close(pipe_fd[0]); // No longer needed
+
+        } else if (kind == EXP_PROC_SUB_IN) {
+            // Redirect child process's input from the reading end of the pipe
+            close(pipe_fd[0]); // Close writing end
+            if (dup2(pipe_fd[1], STDOUT_FILENO) == -1) {
+                perror("dup2");
+                close(pipe_fd[0]);
+                _fatal("dup2: failed", 1);
+            }
+			int fdnull = open("/dev/null", 0666);
+			dup2(fdnull, STDIN_FILENO);
+			close(fdnull);
+            close(pipe_fd[1]); // No longer needed
+        }
+
+        // Execute the provided command
+        shell(SHELL_GET)->interactive = false;
+        parse_input(str, "process_sub", shell_vars);
+        exit_clean();
+    }
+
+	char buffer[100] = {0};
+    // Parent process
+    if (kind == EXP_PROC_SUB_OUT) {
+        // Parent will read from the pipe
+        close(pipe_fd[0]); // Close writing end
+		ft_sprintf(buffer, "/dev/fd/%d", pipe_fd[1]); // Return file descriptor as a string
+		return ft_strdup(buffer);
+    } else if (kind == EXP_PROC_SUB_IN) {
+        // Parent will write to the pipe
+        close(pipe_fd[1]); // Close writing end
+        ft_sprintf(buffer, "/dev/fd/%d", pipe_fd[0]); // Return file descriptor as a string
+		return ft_strdup(buffer);
+    }
+
+    // If something unexpected happens, clean up
+	close(pipe_fd[1]);
+	close(pipe_fd[0]);
+    return NULL;
 }
+
 
 
