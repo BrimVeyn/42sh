50d46fb741b3544e11aac3c186f15d961000a603
BrimVeyn <bryan.vanpaemel@gmail.com>
2025-01-11 17:24:46 +0100

    Fix(tester) Deleted all files generated by the tester and updated redirections format in process substitution filey

--- vm/backend/speed_test.sh
+++ vm/backend/speed_test.sh
@@ -1,7 +1,7 @@
 var=${1}
 start_time=$(date +%s%N) # Initial timestamp in nanoseconds
 
-while true $((--var)) && ! (exit $((var >= 0))); do
+while test "$((var--))" != 0; do
     end_time=$(date +%s%N) # Timestamp at the current iteration
     elapsed=$((end_time - start_time)) # Calculate elapsed time in nanoseconds
 	echo "value: ${var}, elapsed time: $((elapsed / 1000000)) ms"
